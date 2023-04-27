2560f3d49056ba773a8fabf6f218d5333e8f3616
Mike Chugunov <chugunomi@gmail.com>
2023-04-27 16:31:07 +0400

    Fixup

--- Sources/swift-sandbox/main.swift
+++ Sources/swift-sandbox/main.swift
@@ -21,3 +21,15 @@ print("Supported availableOutputSettingsPresets", AVOutputSettingsAssistant.avai
 }()
 
 print("hasHEVCHardwareEncoder", hasHEVCHardwareEncoder)
+
+var systeminfo = utsname()
+uname(&systeminfo)
+let machine = withUnsafeBytes(of: &systeminfo.machine) {bufPtr->String in
+    let data = Data(bufPtr)
+    if let lastIndex = data.lastIndex(where: {$0 != 0}) {
+        return String(data: data[0...lastIndex], encoding: .isoLatin1)!
+    } else {
+        return String(data: data, encoding: .isoLatin1)!
+    }
+}
+print(machine)
